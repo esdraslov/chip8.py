11e48ed558de6ed3dd331dd12e753d6e70e49914
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-10-21 17:48:57 +0000

    chip8 modified and sound, rom created

---
+++ ROMS/tetris.py
@@ -0,0 +1,9 @@
+"""
+a chip8 ROM Game named as tetris
+"""
+class tetris:
+    """
+    do it when left key pressed
+    """
+    def left(self):
+        print("left")
--- chip8.py
+++ chip8.py
@@ -1,1 +1,16 @@
+import keyboard
+from ROMS import tetris
+import os
 
+load: str = None
+ROMS = [
+    "tetris"
+]
+ROM: int = 0
+while True:
+    if load is None:
+        print(f"""
+        >{ROMS[ROM]}<
+
+        -- q - next game --
+        """)
